aad347f6f0e130e4fa3310f0d49a737139a9b91a
Srujan <srujanr40@gmail.com>
2024-08-13 15:54:10 -0700

    Added dark mode toggle

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -1,13 +1,45 @@
-<script>
+<script lang="ts">
 	import '../app.css';
+
+	import { ModeWatcher } from 'mode-watcher';
+
+	import Sun from 'lucide-svelte/icons/sun';
+	import Moon from 'lucide-svelte/icons/moon';
+
+	import { resetMode, setMode } from 'mode-watcher';
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
+	import { Button } from '$lib/components/ui/button/index.js';
 </script>
 
+<ModeWatcher />
+
 <div class="bg-background fixed left-0 right-0 top-0 z-50 mx-auto">
-	<div class="text-foreground mx-auto flex justify-start gap-5 p-5 pl-10">
-		<a class="font-medium leading-3" href="/">Cal</a>
-		<a class="font-medium leading-3" href="/login">Login</a>
-		<a class="font-medium leading-3" href="/register">Register</a>
+	<div class="flex justify-between items-center mx-auto p-2 pl-10">
+		<div class="text-foreground font-medium leading-3">
+			<a href="/">Cal</a>
+		</div>
+		<div class="inline-block pr-3">
+			<DropdownMenu.Root>
+				<DropdownMenu.Trigger asChild let:builder>
+					<Button builders={[builder]} variant="outline" size="icon">
+						<Sun
+							class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+						/>
+						<Moon
+							class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+						/>
+						<span class="sr-only">Toggle theme</span>
+					</Button>
+				</DropdownMenu.Trigger>
+				<DropdownMenu.Content align="end">
+					<DropdownMenu.Item on:click={() => setMode('light')}>Light</DropdownMenu.Item>
+					<DropdownMenu.Item on:click={() => setMode('dark')}>Dark</DropdownMenu.Item>
+					<DropdownMenu.Item on:click={() => resetMode()}>System</DropdownMenu.Item>
+				</DropdownMenu.Content>
+			</DropdownMenu.Root>
+		</div>
 	</div>
+
 	<div class="bg-border h-[0.7px] w-full"></div>
 </div>
 
--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,4 +1,5 @@
-<script>
+<script lang="ts">
+    import { Button } from '$lib/components/ui/button/index.js';
 </script>
 
 <div class="container mx-auto text-center mt-44">
@@ -6,8 +7,11 @@
         <h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-7xl">Plan things</h1>
         <h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-7xl pl-3 text-primary">easier</h1>
     </div>
-    <p class="text-muted-foreground text-xl mt-3">
-        A modal dialog that interrupts the user with important content and expects a
-        response.
+    <p class="text-muted-foreground text-xl mt-6">
+        An intuitive calendar that syncs across accounts, and displays availabilites. <br> Find time in common with people that count. 
     </p>
+    <div class="flex flex-row items-center justify-center mt-7 mx-auto gap-4">
+        <Button href="/login">Get Started</Button>
+        <Button variant="outline">Learn More</Button>
+    </div>
 </div>
